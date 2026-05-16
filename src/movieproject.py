1d095fb9dcfd319ded81c613e22c208c9b9f50da
fishbasquet <demusso.lucio@gmail.com>
2026-05-15 18:47:50 -0600

    Update movie CSV file path for data loading

--- src/movieproject.py
+++ src/movieproject.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-movies = pd.read_csv("movies.csv")
+movies = pd.read_csv("data/movies.csv")
 movies["genres"] = movies["genres"].str.replace("|", " ") # data cleaning for tf-idf (genre vectorization). Since the vertical bar interferes with this process, we replace it with a blank space.
 movies["clean_titles"] = movies["title"].str[:-7].str.lower().str.strip()
 
